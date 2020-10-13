cca43bda8f74d1e594e1b10b990a316787c1e762
Theodor Risager <theodor349@gmail.com>
2020-10-13 18:03:24 +0200

    Added Switch Cases and Parsing for Types

---
+++ Utilities/Converter/Parser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Converter
+{
+    public class Parser
+    {
+        public string TypeToParser(string type, string valueName)
+        {
+            var res = "";
+            switch (type)
+            {
+                case "long":
+                    res = "long.Parse(" + valueName + ")";
+                    break;
+                case "int":
+                    res = "int.Parse(" + valueName + ")";
+                    break;
+                case "float":
+                    res = "float.Parse(" + valueName + ")";
+                    break;
+                case "decimal":
+                    res = "decimal.Parse(" + valueName + ")";
+                    break;
+                case "DateTime":
+                    res = "DateTime.Parse(" + valueName + ", CultureInfo.CurrentCulture)";
+                    break;
+                case "bool":
+                    res = "bool.Parse(" + valueName + ")";
+                    break;
+                default:
+                    res = valueName;
+                    break;
+            }
+            return res;
+        }
+    }
+}
--- Utilities/StringConcatination/Concattor.cs
+++ Utilities/StringConcatination/Concattor.cs
@@ -1,4 +1,5 @@
-﻿using FileAccess;
+﻿using Converter;
+using FileAccess;
 using FileAccess.DataTypes;
 using System;
 using System.Collections.Generic;
@@ -9,26 +10,43 @@ namespace StringConcatination
     {
         public void Run()
         {
+            var converter = new Parser();
+
             string path = "";
             var data = new DataAccess();
             var records = data.Load<DoubleValue<string, string>>(path + "Input.txt");
-            var outPut = new List<SingleValue<string>>();
+            records.ForEach(x => x.Item2 = converter.TypeToParser(x.Item2, "value"));
+            var outPut = MakeSwitchCases(records, "car");
+            data.Write(path + "Output.txt", outPut);
+        }
 
+        public List<SingleValue<string>> MakeSwitchCases(List<DoubleValue<string, string>> records, string variableName)
+        {
+            var outPut = new List<SingleValue<string>>();
+            Set(ref outPut, i => "case (\"", records.Count);
+            Append(ref outPut, i => records[i].Item1 + "\"):\n");
+            Append(ref outPut, i => "\t" + variableName + "." + records[i].Item1 + " = " + records[i].Item2 + ";\n");
+            Append(ref outPut, i => "\tbreak;");
+            return outPut;
+        }
+
+        public List<SingleValue<string>> MakePublicVariable(List<DoubleValue<string, string>> records)
+        {
+            var outPut = new List<SingleValue<string>>();
             Set(ref outPut, i => "public ", records.Count);
             Append(ref outPut, i => records[i].Item2 + " ");
             Append(ref outPut, i => records[i].Item1 + " ");
             Append(ref outPut, i => "{ get; set; }");
-
-            data.Write(path + "Output.txt", outPut);
+            return outPut;
         }
 
-        private void Append(ref List<SingleValue<string>> list, Func<int, string> f)
+        public void Append(ref List<SingleValue<string>> list, Func<int, string> f)
         {
             for (int i = 0; i < list.Count; i++)
                 list[i].Item1 += f(i);
         }
 
-        private void Set(ref List<SingleValue<string>> list, Func<int, string> f, int length)
+        public void Set(ref List<SingleValue<string>> list, Func<int, string> f, int length)
         {
             for (int i = 0; i < length; i++)
                 list.Add(new SingleValue<string>() { Item1 = f(i) });
